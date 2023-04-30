3143e759f150dafd9347a5ed2652eae8a5cdef5d
KPMGE <jesuskevin254@gmail.com>
2023-04-30 11:05:00 -0300

    :sparkles: add simple healthcheck endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, Responder, HttpServer, App, HttpResponse};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()>{
+    HttpServer::new(|| {
+        App::new()
+            .service(healthcheck)
+    })
+    .bind(("localhost", 8080))?
+    .run()
+    .await
+}
+
+#[get("/healthcheck")]
+async fn healthcheck() -> impl Responder {
+    HttpResponse::Ok()
 }
