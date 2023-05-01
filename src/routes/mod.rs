b272b08307cbdee88d2e21a1550448b5a186f942
KPMGE <jesuskevin254@gmail.com>
2023-05-01 18:17:31 -0300

    :recycle: refactor modules

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,2 +1,5 @@
 pub mod health_check;
 pub mod subscriptions;
+
+pub use health_check::*;
+pub use subscriptions::*;
--- src/startup.rs
+++ src/startup.rs
@@ -2,8 +2,7 @@ use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{HttpServer, App};
 
-use crate::routes::health_check::health_check;
-use crate::routes::subscriptions::subscribe;
+use crate::routes::{health_check, subscribe};
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
