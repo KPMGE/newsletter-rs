1349e0a606f4c3fdd9a76ac087c8cff2c73a8355
KPMGE <jesuskevin254@gmail.com>
2023-05-06 12:11:36 -0300

    :recycle: connect to the database lazily

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use newsletter_rs::telemetry::{get_subscriber, init_subscriber};
 use newsletter_rs::{configuration, startup::run};
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
+use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -11,9 +12,11 @@ async fn main() -> std::io::Result<()> {
 
     let configs = configuration::get_configuration().expect("could not read configuration file!");
     let db_connection_string = configs.database.get_connection_string();
-    let pool = PgPool::connect(&db_connection_string.expose_secret())
-        .await
-        .expect("Failed to connect to Postgres");
+    let pool = PgPoolOptions::new()
+        .connect_timeout(Duration::from_secs(2))
+        .connect_lazy(&db_connection_string.expose_secret())
+        .expect("could not connect to the database");
+
 
     let address = format!("{}:{}", configs.app.host, configs.app.port);
     let listener = TcpListener::bind(address).expect("could not start tcp listener");
