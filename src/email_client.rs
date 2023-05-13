332d447fbb1c496b8946d51e6abe415cbb6e5750
KPMGE <jesuskevin254@gmail.com>
2023-05-13 12:10:24 -0300

    :white_check_mark: ensure email client returns error if email server returns error

--- src/email_client.rs
+++ src/email_client.rs
@@ -51,7 +51,8 @@ impl EmailClient {
             .header("X-Postmark-Server-Token", self.authorization_token.expose_secret())
             .json(&request_body)
             .send()
-            .await?;
+            .await?
+            .error_for_status()?;
 
         Ok(())
     }
@@ -64,8 +65,8 @@ mod tests {
     use fake::{faker::internet::en::SafeEmail, Fake};
     use secrecy::Secret;
     use wiremock::{MockServer, Mock, ResponseTemplate};
-    use wiremock::matchers::{header_exists, header, method, path};
-    use claim::assert_ok;
+    use wiremock::matchers::{header_exists, header, method, path, any};
+    use claim::{assert_ok, assert_err};
     use super::EmailClient;
     use crate::domain::SubscriberEmail;
 
@@ -109,5 +110,24 @@ mod tests {
 
         let result = email_client.send_email(subscriber_email, &subject, &content, &content).await;
         assert_ok!(result);
+    }
+
+    #[tokio::test]
+    async fn send_email_fails_if_server_returns_500() {
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake()));
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(500))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let result = email_client.send_email(subscriber_email, &subject, &content, &content).await;
+        assert_err!(result);
     }
 }
