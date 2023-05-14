9857c8f6b3fbc71a75f3b6abb82fe38242a6cf02
KPMGE <jesuskevin254@gmail.com>
2023-05-14 13:20:06 -0300

    :recycle: move post subscriptions logic to its own helper function

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -3,8 +3,6 @@ use super::helpers::spawn_app;
 #[tokio::test]
 async fn subscribe_returns_400_when_data_is_missing() {
     let app = spawn_app().await;
-    let client = reqwest::Client::new();
-
     let test_cases = vec![
         ("name=testname", "missing name"),
         ("name=testmail", "missing email"),
@@ -12,14 +10,7 @@ async fn subscribe_returns_400_when_data_is_missing() {
     ];
 
     for (invalid_body, error_message) in test_cases {
-        let response = client
-            .post(format!("{}/subscribe", app.address))
-            .header("Content-Type", "x-www-urlencoded")
-            .body(invalid_body)
-            .send()
-            .await
-            .expect("Failed to execute request!");
-
+        let response = app.post_subscriptions(invalid_body.to_string()).await;
         assert_eq!(
             response.status().as_u16(),
             400,
@@ -32,16 +23,8 @@ async fn subscribe_returns_400_when_data_is_missing() {
 #[tokio::test]
 async fn subscribe_returns_200_for_valid_form_data() {
     let app = spawn_app().await;
-    let client = reqwest::Client::new();
-
-    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-    let response = client
-        .post(format!("{}/subscribe", app.address))
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(body)
-        .send()
-        .await
-        .expect("Failed to execute request");
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com".to_string();
+    let response = app.post_subscriptions(body).await;
 
     let data_saved = sqlx::query!("SELECT email, name FROM subscriptions")
         .fetch_one(&app.db_pool)
@@ -56,16 +39,7 @@ async fn subscribe_returns_200_for_valid_form_data() {
 #[tokio::test]
 async fn suscribe_returns_400_when_data_is_present_but_invalid() {
     let app = spawn_app().await;
-    let client = reqwest::Client::new();
-    let body = "name=()<>=ursula_le_guin%40gmail.com";
-
-    let response = client
-        .post(format!("{}/subscribe", app.address))
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(body)
-        .send()
-        .await
-        .expect("Failed to execute request");
-
+    let body = "name=()<>=ursula_le_guin%40gmail.com".to_string();
+    let response = app.post_subscriptions(body).await;
     assert_eq!(400, response.status());
 }
